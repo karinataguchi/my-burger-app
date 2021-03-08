94e2b32a78911e503a1a002123dcbe093de0ba43
Karina Taguchi <karina.taguchi@gmail.com>
2021-03-08 10:02:22 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
@@ -8,7 +8,8 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder></BurgerBuilder>
+          < BurgerBuilder />
+          <p>Test git commit</p>
         </Layout>
       </div>
     );
--- src/hoc/Aux/Aux.js
+++ src/hoc/Aux/Aux.js
@@ -1,3 +1,6 @@
+import React from 'react';
+
+
 const aux = (props) => (props.children);
 
 export default aux;
